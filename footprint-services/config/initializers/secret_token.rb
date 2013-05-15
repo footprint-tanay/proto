11899293b7ba07437b5b52264e81f5672a0e9084
Tanay Jayawant <tanay@tanay.local>
2013-05-15 15:38:58 -0400

    first commit

---
+++ footprint-services/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FootprintServices::Application.config.secret_key_base = '8832ba54eb89db64dee106be7ee9b906e5363fc1c0d4b86e670dd3baf08534a2495051c1a0a82c384361f48c7fb77b6da0614a4327ffb99e7d6d86e192891d50'
---
+++ footprint-services/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FootprintServices::Application.config.session_store :cookie_store, key: '_footprint-services_session'
